ce331594ce5b2ef8d35706792967688cc62f1059
Akshaymeshram01 <akshaymesh23@gmail.com>
2026-08-06 22:48:58 +0530

    i have completed the hero section and navbar

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,5 +1,6 @@
 import FeaturedHotels from './components/FeaturedHotels'
-import HeroSection from './components/HeroSection'
+import Navbar from "../../components/layout/Navbar.jsx";
+import HeroSection from './components/HeroSection.jsx'
 import PopularDestinations from './components/PopularDestinations'
 import PromoBanner from './components/PromoBanner'
 import TourGuide from './components/TourGuide'
@@ -7,6 +8,7 @@ import TourGuide from './components/TourGuide'
 function HomePage() {
   return (
     <>
+      <Navbar />
       <HeroSection />
       <FeaturedHotels />
       <TourGuide/>
